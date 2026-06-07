602ccd6578a0bb563d744fb1335aff34af478e8b
William Alves <william.bas@live.com>
2026-06-06 22:36:35 -0300

    Adicionei um menu e coloquei uma estrutura de decisões aninhadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,6 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
@@ -18,6 +17,19 @@ int main() {
     float super1;
     float super2;
     int resultado;
+    int menu;
+
+  // Menu do Jogo
+  printf ("Bem-vindo ao Super Trunfo - Cidades!\n");
+  printf ("Escolha um Poder:\n");
+  printf ("1. População\n");
+  printf ("2. Área\n");
+  printf ("3. PIB\n");
+  printf ("4. Turismo\n");
+  printf ("5. Densidade Populacional\n");
+  printf ("6. PIB per Capita\n");
+  printf ("7. Super Poder\n");
+  scanf ("%d", &menu);
 
   // Área para entrada de dados
   //Carta 1
@@ -58,7 +70,7 @@ int main() {
   //Carta 1
   densidade1 = populacao1/area1;
   pibperca1 = pib1/populacao1;
-  super1 = populacao1 + area1 + pib1 + turismo1 + pibperca1 + (1 / densidade1);
+  super1 = populacao1 + area1 + pib1 + turismo1 + pibperca1 + (1.0 / densidade1);
   printf ("\nResultado da primeira carta \n");
   printf ("Estado: %s - Código: %s\n", estado1, codigo1);
   printf ("Cidade: %s - População: %d\n", cidade1, populacao1);
@@ -72,7 +84,7 @@ int main() {
   //Carta 2
   densidade2 = populacao2/area2;
   pibperca2 = pib2/populacao2;
-  super2 = populacao2 + area2 + pib2 + turismo2 + pibperca2 + (1 / densidade2);
+  super2 = populacao2 + area2 + pib2 + turismo2 + pibperca2 + (1.0 / densidade2);
   printf ("\nResultado da segunda carta \n");
   printf ("Estado: %s - Código: %s\n", estado2, codigo2);
   printf ("Cidade: %s - População: %d\n", cidade2, populacao2);
@@ -84,14 +96,74 @@ int main() {
   printf ("Super Poder: %.2f\n", super2);
 
   // Área para comparação das cartas
-  if ( super1 > super2)
-  {
-    printf ("\nA carta 1 é a vencedora!\n");
+  switch (menu) {
+    case 1:
+      if (populacao1 > populacao2) {
+        printf ("A carta 1 é a vencedora!\n");
+      } else if (populacao1 < populacao2) {
+        printf ("A carta 2 é a vencedora!\n");
+      } else {
+        printf ("Empate!\n");
+      }
+      break;
+    case 2:
+      if (area1 > area2) {
+        printf ("A carta 1 é a vencedora!\n");
+      } else if (area1 < area2) {
+        printf ("A carta 2 é a vencedora!\n");
+      } else {
+        printf ("Empate!\n");
+      }
+      break;
+    case 3:
+      if (pib1 > pib2) {
+        printf ("A carta 1 é a vencedora!\n");
+      } else if (pib1 < pib2) {
+        printf ("A carta 2 é a vencedora!\n");
+      } else {
+        printf ("Empate!\n");
+      }
+      break;
+    case 4:
+      if (turismo1 > turismo2) {
+        printf ("A carta 1 é a vencedora!\n");
+      } else if (turismo1 < turismo2) {
+        printf ("A carta 2 é a vencedora!\n");
+      } else {
+        printf ("Empate!\n");
+      }
+      break;
+    case 5:
+      if (densidade1 < densidade2) {
+        printf ("A carta 1 é a vencedora!\n");
+      } else if (densidade1 > densidade2) {
+        printf ("A carta 2 é a vencedora!\n");
+      } else {
+        printf ("Empate!\n");
+      }
+      break;
+    case 6:
+      if (pibperca1 > pibperca2) {
+        printf ("A carta 1 é a vencedora!\n");
+      } else if (pibperca1 < pibperca2) {
+        printf ("A carta 2 é a vencedora!\n");
+      } else {
+        printf ("Empate!\n");
+      }
+      break;
+    case 7:
+      if (super1 > super2) {
+        printf ("A carta 1 é a vencedora!\n");
+      } else if (super1 < super2) {
+        printf ("A carta 2 é a vencedora!\n");
+      } else {
+        printf ("Empate!\n");
+      }
+      break;
+    default:
+      printf ("Você não escolheu uma opção válida. Tente novamente.\n");
   }
-  else (super1<super2);{
-    printf ("\nA carta 2 é a vencedora!\n");
-  }
-  
+
   
 return 0;
 } 
